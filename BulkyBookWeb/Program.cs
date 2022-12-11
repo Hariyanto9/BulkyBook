cd106bdb96d466dd37f56c34e85fcc3633591570
Hariyanto <hariyanto@deosgroup.id>
2022-12-11 17:49:23 +0700

    add bootstraps theme

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BulkyBookWeb.Data.ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation(); //refresh view jika ada perubahan
 
 var app = builder.Build();
 
